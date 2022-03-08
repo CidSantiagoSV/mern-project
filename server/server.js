c279c2f5a90b8055c8c4febe707962a60d253d18
CidSantiagoSV <cid.santiago1@gmail.com>
2022-03-07 22:19:19 -0500

    Connections established

---
+++ server/server.js
@@ -0,0 +1,28 @@
+﻿// required stuff to run the server
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+
+//hide enviromental variables
+require('dotenv').config();
+
+//create express server
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+//cors middleware || parse json
+app.use(cors());
+app.use(express.json());
+
+//connect to mongoDB
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true });
+const connection = mongoose.connection;
+    console.log("MongoDB connection established successfully");
+
+//starts the server
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
